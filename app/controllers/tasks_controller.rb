67481d303a8106b22c93874647fef18087b0c99a
Dimetriu <dima.rashidov@gmail.com>
2017-01-30 21:16:57 +0200

    Customized controller Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, on: [:show, :edit, :update, :destroy]
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   # GET /tasks
   # GET /tasks.json
   def index 
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   # GET /tasks/1
@@ -19,21 +22,22 @@ class TasksController < ApplicationController
 
   # GET /tasks/1/edit
   def edit
+    @projects = Project.all.order(created_at: :desc)
   end
 
   # POST /tasks
   # POST /tasks.json
-  def create
-    @task = Task.new(task_params)
-
+  def create 
+    @task = @project.tasks.create(task_params)
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        format.html { redirect_to project_path }
+        format.json { render :show, status: :created, location: @project }
+        flash[:success] = "Task was created! You can #{make_undo_link} this action"
       else
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+      end     
     end
   end
 
@@ -42,32 +46,39 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        format.html { redirect_to @project }
+        format.json { render :show, status: :ok, location: @project }
+        flash[:success] = "Task was updated! You can #{make_undo_link} this action"
       else
         format.html { render :edit }
         format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+      end 
     end
   end
 
   # DELETE /tasks/1
   # DELETE /tasks/1.json
-  def destroy
+ def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to @project }
       format.json { head :no_content }
-    end
+      flash[:success] = "Task was deleted! You can #{make_undo_link} this action"
+    end   
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+
+    
+
     def task_params
       params.require(:task).permit(:content, :date, :priority, :done)
     end
